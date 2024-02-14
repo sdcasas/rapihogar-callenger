c3ea1ad8b294cd599f1b9d612c6ea1ed95315662
Sergio Daniel <sdcasas@gmail.com>
2024-02-14 08:32:29 -0300

    ADD api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from core.models import Company
-
-from .views import CompanyViewSet
+from .views import CompanyViewSet, TecnicoList, PedidoViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'company', CompanyViewSet, basename='company')
 
+router.register(r'company', CompanyViewSet, basename='company')
+router.register(r'v1/pedido', PedidoViewSet, basename='pedido_update')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('v1/tecnicos', TecnicoList.as_view()),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,10 @@
-from rest_framework import viewsets, permissions, serializers
+from rest_framework import viewsets, serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Value
+from django.db.models.functions import Concat
 
-from core.models import Company
+from core.models import Company, Tecnico, Pedido
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -12,4 +16,36 @@ class CompanySerializer(serializers.ModelSerializer):
 
 class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
-    queryset = Company.objects.filter()
+    queryset = Company.objects.all()
+
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
+
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    serializer_class = PedidoSerializer
+    queryset = Pedido.objects.all()
+
+
+class TecnicoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Tecnico
+        fields = ['full_name', 'hours_worked', 'total_pedidos', 'total_payment']
+
+
+class TecnicoList(APIView):
+
+    def get(self, request, *args, **kwargs):
+        tecnico_query = Tecnico.objects.all()
+        query = self.request.query_params.get('query', None)
+        if query:
+            tecnico_query = (Tecnico.objects
+                                    .annotate(fullname_search=Concat('apellido', Value(' '), 'nombre'))
+                                    .filter(fullname_search__icontains=query))
+        serializer = TecnicoSerializers(tecnico_query, many=True)
+        return Response(serializer.data)
+    
+
