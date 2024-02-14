264fb37d1536caab7155611ec380d27f7ae635c4
Sergio Daniel <sdcasas@gmail.com>
2024-02-14 11:24:37 -0300

    CHANGE basename url

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -7,7 +7,7 @@ from .views import CompanyViewSet, TecnicoList, PedidoViewSet
 router = routers.DefaultRouter()
 
 router.register(r'company', CompanyViewSet, basename='company')
-router.register(r'v1/pedido', PedidoViewSet, basename='pedido_update')
+router.register(r'v1/pedido', PedidoViewSet, basename='pedido')
 
 urlpatterns = [
     path('', include(router.urls)),
